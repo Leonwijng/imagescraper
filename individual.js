22dc3a543ca646a991d5125cb217bcdd1b21d95d
501urchin <anish.chittu2007@gmail.com>
2025-11-13 22:49:32 +0100

    adsfvdf

---
+++ individual.js
@@ -0,0 +1,26 @@
+fetch(
+  "https://app.meijertheoriecursus.nl/app/videocursus/pttnxpd59w75se3/e6j236tc164mxzj/93psyvanypegrj9/__data.json?x-sveltekit-invalidated=00011",
+  {
+    headers: {
+      accept: "*/*",
+      "accept-language": "en-US,en;q=0.9",
+      "cache-control": "no-cache",
+      pragma: "no-cache",
+      priority: "u=1, i",
+      "sec-ch-ua":
+        '"Chromium";v="142", "Google Chrome";v="142", "Not_A Brand";v="99"',
+      "sec-ch-ua-mobile": "?0",
+      "sec-ch-ua-platform": '"Windows"',
+      "sec-fetch-dest": "empty",
+      "sec-fetch-mode": "cors",
+      "sec-fetch-site": "same-origin",
+    },
+    referrer: "https://app.meijertheoriecursus.nl/app/videocursus",
+    body: null,
+    method: "GET",
+    mode: "cors",
+    credentials: "include",
+  }
+);
+
+
---
+++ rss.js
@@ -0,0 +1,26 @@
+fetch(
+  "https://app.meijertheoriecursus.nl/_app/remote/cxfi27/getVideoCoursesWithProgress",
+  {
+    headers: {
+      accept: "*/*",
+      "accept-language": "en-US,en;q=0.9",
+      "cache-control": "no-cache",
+      pragma: "no-cache",
+      priority: "u=1, i",
+      "sec-ch-ua":
+        '"Chromium";v="142", "Google Chrome";v="142", "Not_A Brand";v="99"',
+      "sec-ch-ua-mobile": "?0",
+      "sec-ch-ua-platform": '"Windows"',
+      "sec-fetch-dest": "empty",
+      "sec-fetch-mode": "cors",
+      "sec-fetch-site": "same-origin",
+      "x-sveltekit-pathname": "/app/videocursus",
+      "x-sveltekit-search": "",
+    },
+    referrer: "https://app.meijertheoriecursus.nl/app/videocursus",
+    body: null,
+    method: "GET",
+    mode: "cors",
+    credentials: "include",
+  }
+);
